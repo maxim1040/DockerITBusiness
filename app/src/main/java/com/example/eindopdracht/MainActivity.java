fa3f52cd7d46e675a29beac6f228111d8d075112
maxim1040 <maxim.sbk1040@gmail.com>
2025-01-29 16:02:12 +0100

    ci cd test2

--- app/src/main/java/com/example/eindopdracht/MainActivity.java
+++ app/src/main/java/com/example/eindopdracht/MainActivity.java
@@ -15,7 +15,7 @@ import com.example.eindopdracht.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
+//ci cd test 1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
